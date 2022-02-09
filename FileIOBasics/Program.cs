29ae6dc65fcc184822adfb43b7fc956d2015b1fc
srk-333 <sauravmk333@gmail.com>
2022-02-09 18:40:25 +0530

    [Saurav]Add:Welcome message

---
+++ FileIOBasics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOBasics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to File Io Operations");
+            Console.ReadKey();
+        }
+    }
+}
